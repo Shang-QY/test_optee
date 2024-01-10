559143abc89307d0a4df58978a586524267330ad
Yong Li <yong.li@intel.com>
2024-01-10 14:45:35 +0800

    Update for run script

--- run-term-gdb.sh
+++ run-term-gdb.sh
@@ -1,10 +1,10 @@
 ./qemu/build/qemu-system-riscv64 -d guest_errors -D guest_log.txt \
-    -M virt,pflash0=pflash0,aia=aplic-imsic,acpi=off,hmat=on,rpmi=on \
+    -M virt,aia=aplic-imsic,acpi=off,hmat=on,rpmi=on \
     -dtb ./qemu-virt-new.dtb \
     -m 4G,slots=2,maxmem=8G -object memory-backend-ram,size=2G,id=m0 -object memory-backend-ram,size=2G,id=m1 \
     -numa node,nodeid=0,memdev=m0 -numa node,nodeid=1,memdev=m1 -smp 2,sockets=2,maxcpus=2 \
     -bios ./fw_dynamic.elf \
+    -kernel ./u-boot/u-boot.bin \
     -device loader,file=tee-pager_v2.bin,addr=0x80C00000 \
-    -blockdev node-name=pflash0,driver=file,read-only=on,filename=./u-boot.bin \
     -drive file=fat:rw:~/src/fat,id=hd0 -device virtio-blk-device,drive=hd0 \
     -nographic -s -S
