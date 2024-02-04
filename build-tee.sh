f143e9599051a216cf1eada6839187ea0c5e13a2
Yong Li <yong.li@intel.com>
2024-02-04 09:31:29 +0800

    Switch to opensource optee

--- build-tee.sh
+++ build-tee.sh
@@ -1,23 +1,32 @@
 export WORKDIR=`pwd`
 loopdevice=`sudo losetup --partscan --find --show disk.img`
 cd ./optee_client/build
-cmake -DCMAKE_C_COMPILER=riscv64-linux-gnu-gcc -DCMAKE_INSTALL_PREFIX=./out/export/usr ..
+cmake -DCMAKE_C_COMPILER=/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc -DCMAKE_INSTALL_PREFIX=./out/export/usr .. clean
+cmake -DCMAKE_C_COMPILER=/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc -DCMAKE_INSTALL_PREFIX=./out/export/usr ..
 make
 make install
 cd -
 cd optee_examples/hello_world/host
 make \
-    CROSS_COMPILE=riscv64-linux-gnu- \
+    CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- \
+    TEEC_EXPORT=$WORKDIR/optee_client/build/out/export/usr \
+    --no-builtin-variables clean
+make \
+    CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- \
     TEEC_EXPORT=$WORKDIR/optee_client/build/out/export/usr \
     --no-builtin-variables
 cd -
 cd optee_examples/hello_world/ta
 make \
-    CROSS_COMPILE=riscv64-linux-gnu- \
+    CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- \
     PLATFORM=vexpress-qemu_virt \
-    TA_DEV_KIT_DIR=$WORKDIR/optee_os/out/riscv-plat-nuclei/export-ta_rv64
-
+    TA_DEV_KIT_DIR=$WORKDIR/optee_os/out/riscv-plat-virt/export-ta_rv64 clean
+make \
+    CROSS_COMPILE=/opt/riscv/bin/riscv64-unknown-linux-gnu- \
+    PLATFORM=vexpress-qemu_virt \
+    TA_DEV_KIT_DIR=$WORKDIR/optee_os/out/riscv-plat-virt/export-ta_rv64
 cd -
+/opt/riscv/bin/riscv64-unknown-linux-gnu-objdump -t -S ./optee_examples/hello_world/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.elf > $WORKDIR/8aaaf200-2450-11e4-abe2-0002a5d5c51b.txt
 echo $loopdevice
 sudo mount ${loopdevice}p1 ./mnt
 sudo cp -rf ./optee_client/build/out/export/usr/* ./mnt/usr/
--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ cp build/platform/generic/firmware/fw_dynamic.elf $WORKDIR
 riscv64-linux-gnu-objdump -t -S build/platform/generic/firmware/fw_dynamic.elf > $WORKDIR/fw_dynamic.txt
 cd $WORKDIR
 cd optee_os
-make CFG_TEE_CORE_LOG_LEVEL=3 CROSS_COMPILE64=riscv64-linux-gnu- ARCH=riscv CFG_RV64_core=y CFG_TZDRAM_START=0xF0C00000 CFG_TZDRAM_SIZE=0x800000 CFG_SHMEM_START=0xF1600000 CFG_SHMEM_SIZE=0x200000 PLATFORM=nuclei ta-targets=ta_rv64 MARCH=rv64imafdc MABI=lp64d
+make CFG_TEE_CORE_LOG_LEVEL=3 CROSS_COMPILE64=/opt/riscv/bin/riscv64-unknown-linux-gnu- ARCH=riscv CFG_DT=n CFG_RV64_core=y CFG_TDDRAM_START=0xF0C00000 CFG_TDDRAM_SIZE=0x800000 CFG_SHMEM_START=0xF1600000 CFG_SHMEM_SIZE=0x200000 PLATFORM=virt ta-targets=ta_rv64 MARCH=rv64imafdc MABI=lp64d
 
-cp out/riscv-plat-nuclei/core/tee-pager_v2.bin $WORKDIR
-riscv64-linux-gnu-objdump -t -S out/riscv-plat-nuclei/core/tee.elf > $WORKDIR/tee.txt
+cp out/riscv-plat-virt/core/tee.bin $WORKDIR/tee-pager_v2.bin
+/opt/riscv/bin/riscv64-unknown-linux-gnu-objdump -t -S out/riscv-plat-virt/core/tee.elf > $WORKDIR/tee.txt
+/opt/riscv/bin/riscv64-unknown-linux-gnu-objdump -t -S out/riscv-plat-virt/ldelf/ldelf.elf > $WORKDIR/ldelf.txt
