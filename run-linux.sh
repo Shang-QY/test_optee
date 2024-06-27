db0277cf088fa5e4761603a11a6a18c8b3e7ca07
Qingyu Shang <2931013282@sjtu.edu.cn>
2024-06-27 16:44:46 +0800

    refine run script
    Signed-off-by: Qingyu Shang <2931013282@sjtu.edu.cn>

--- run-linux.sh
+++ run-linux.sh
@@ -3,10 +3,10 @@
 # while ! nc -z 127.0.0.1 54320 || ! nc -z 127.0.0.1 54321; do sleep 1; done
 ./qemu/build/qemu-system-riscv64 -d guest_errors -D guest_log.txt \
     -M virt,aia=aplic-imsic,acpi=off,hmat=on,rpmi=on \
-    -dtb ./qemu-virt-new.dtb \
     -m 4G,slots=2,maxmem=8G -object memory-backend-ram,size=2G,id=m0 -object memory-backend-ram,size=2G,id=m1 \
     -numa node,nodeid=0,memdev=m0 -numa node,nodeid=1,memdev=m1 -smp 2,sockets=2,maxcpus=2 \
-    -bios ./fw_dynamic.elf \
+    -dtb ./qemu-virt-new.dtb \
+    -bios ./opensbi/build/platform/generic/firmware/fw_dynamic.elf \
     -kernel ./u-boot/u-boot.bin \
     -device loader,file=tee-pager_v2.bin,addr=0xF0C00000 \
     -serial tcp:localhost:54320,server \
